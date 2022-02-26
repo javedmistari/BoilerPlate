70b7e7d136df5d3339cc90eda4f400258110f450
Javed Mistari <javed.mistari@webmaffia.com>
2022-02-26 22:38:32 +0530

    Added all files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<?php require_once 'head.php'; ?>
+
+	<title></title>
+
+</head>
+<body>
+	<?php require_once 'header.php'; ?>
+
+	<main class="wrapper">
+
+	</main>
+
+	<?php require_once 'footer.php'; ?>
+</body>
+</html>
